2b14ec3738f08eb739304fbf9bce95005bbf035d
PPrydorozhnyi <Peter35128@gmail.com>
2020-11-15 20:15:21 +0200

    parse query params

--- backend/src/main/java/com/aws/dynamodb/functions/DynamoGetChangeLogsFunction.java
+++ backend/src/main/java/com/aws/dynamodb/functions/DynamoGetChangeLogsFunction.java
@@ -2,20 +2,25 @@ package com.aws.dynamodb.functions;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
-import com.aws.dynamodb.model.ChangeLogRequestDTO;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.aws.dynamodb.services.DaoService;
 import com.aws.dynamodb.services.DaoServiceImpl;
 import java.util.List;
 import java.util.Map;
+import lombok.extern.log4j.Log4j;
 
+@Log4j
 public class DynamoGetChangeLogsFunction
-    implements RequestHandler<ChangeLogRequestDTO, List<Map<String, Object>>> {
+    implements RequestHandler<APIGatewayProxyRequestEvent, List<Map<String, Object>>> {
 
   private final DaoService daoService = DaoServiceImpl.service();
 
   @Override
-  public List<Map<String, Object>> handleRequest(ChangeLogRequestDTO changeLogRequestDTO,
+  public List<Map<String, Object>> handleRequest(APIGatewayProxyRequestEvent changeLogRequestDTO,
                                                  Context context) {
-    return daoService.getChangeLogs(changeLogRequestDTO.getId(), changeLogRequestDTO.getVersion());
+    log.debug("Proxy request:" + changeLogRequestDTO);
+    final String changelogId = changeLogRequestDTO.getPathParameters().get("changelog_id");
+    final String[] split = changelogId.split(":");
+    return daoService.getChangeLogs(split[0], split[1]);
   }
 }
